7ad4ab099e56b53d8285f03fe820b0b1e15fee24
Samba NDIAYE <ndiayesamba1996@gmail.com>
2023-05-16 13:23:08 +0000

    README.md

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,43 +1,32 @@
 #!/usr/bin/python3
 """
-Making Change
+Given a pile of coins of different values, determine the
+fewest number of coins needed to meet a given amount total.
 """
 
 
 def makeChange(coins, total):
     """
-    Return the minimum number of coins needed to meet a given total
+    Determines the fewest coins to meet total
+
     Args:
-        coins (list of ints): a list of coins of different values
-        total (int): total value to be met
-    Return:
-        Number of coins or -1 if meeting the total is not possible
+        coins (list): list of coins
+        total (int): value to meet as total
+
+    Returns:
+        coins (int): fewest number of coins needed to meet total
+        (0) if total is 0 or less
+        (-1) if no number of the coins can meet the total
     """
     if total <= 0:
         return 0
-    if coins == [] or coins is None:
+    min_coins = [0] + [float('inf')] * total
+    for i in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+                num_coins = min_coins[i - coin] + 1
+                if num_coins < min_coins[i]:
+                    min_coins[i] = num_coins
+    if min_coins[-1] == float("inf"):
         return -1
-    try:
-        n = coins.index(total)
-        return 1
-    except ValueError:
-        pass
-
-    coins.sort(reverse=True)
-    coin_count = 0
-    for i in coins:
-        if total % i == 0:
-            coin_count += int(total / i)
-            return coin_count
-        if total - i >= 0:
-            if int(total / i) > 1:
-                coin_count += int(total / i)
-                total = total % i
-            else:
-                coin_count += 1
-                total -= i
-                if total == 0:
-                    break
-    if total > 0:
-        return -1
-    return coin_count
+    return min_coins[total]
